d238bb5c7e6c4d83b39be3ef9087cf11573b3692
LizethVictoria20 <lizvictoria754@gmail.com>
2020-09-14 22:30:38 +0000

    Export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Export to JSON"""
+import json
+import requests
+import sys
+
+
+def f_json(users=None, todos=None):
+    """Export to CSV"""
+    data = []
+    with open(sys.argv[1] + ".json", "w") as file:
+        for i in todos:
+            data.append({"task": i.get("title"),
+                         "completed": i.get("completed"),
+                         "username": users[0].get("username")})
+            json.dump(data, file)
+
+if __name__ == "__main__":
+    """[Function initial]"""
+    if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        args_id = {"id": sys.argv[1]}
+        users = requests.get("https://jsonplaceholder.typicode.com/users",
+                             params=args_id).json()
+        args_userid = {"userId": sys.argv[1]}
+        todos = requests.get("https://jsonplaceholder.typicode.com/todos",
+                             params=args_userid).json()
+        done_task = 0
+        number_task = []
+        for i in todos:
+            if i.get("completed"):
+                number_task.append(i)
+                done_task += 1
+            f_json(users, todos)
