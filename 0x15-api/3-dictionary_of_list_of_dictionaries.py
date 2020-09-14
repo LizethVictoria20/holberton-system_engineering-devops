b7cdd619d7d8c4e7a6a440138228978b616ae28c
LizethVictoria20 <lizvictoria754@gmail.com>
2020-09-14 23:52:55 +0000

    Update

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -2,28 +2,26 @@
 """Dictionary of list of dictionaries """
 import json
 import requests
-import sys
 
 
-def f_json(users=None, todos=None):
-    """Export to CSV"""
-    data = []
-    user_id = {}
-    with open("todo_all_employees.json", "w") as file:
+def make_all(users=None, todos=None):
+    """Turns all payloads into JSON format"""
+    all_list = []
+    alljson = {}
+    with open("todo_all_employees.json", "w") as f:
         for i in users:
-            user = i.get("id")
+            u = i.get("id")
             for i in todos:
-                if user == i.get("userId"):
-                    data.append({"username": users[0].get("username"),
-                                 "task": i.get("title"),
-                                 "completed": i.get("completed")})
+                if u == i.get("userId"):
+                    all_list.append({"username": users[0].get("username"),
+                                     "task": i.get("title"),
+                                     "completed": i.get("completed")})
+            alljson[u] = all_list
+        json.dump(alljson, f)
 
-            user_id[user] = data
-        json.dump(user_id, file)
 
 if __name__ == "__main__":
-    """[Function initials]"""
     users = requests.get("https://jsonplaceholder.typicode.com/users/").json()
     todos = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
 
-    f_json(users, todos)
+    make_all(users, todos)
