daf53432ba3ff0dc7229dcf0f859e1adc5c97410
LizethVictoria20 <lizvictoria754@gmail.com>
2020-09-14 23:31:59 +0000

    Update

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,21 +7,22 @@ import sys
 
 def f_json(users=None, todos=None):
     """Export to CSV"""
-    list_json = []
-    data = {}
-    with open("todo_all_employees.json", "w") as file:
+    data = []
+    list_json = {}
+    with open("todo_all_employees.json", "w") as f:
         for i in users:
             u = i.get("id")
             for i in todos:
                 if u == i.get("userId"):
-                    list_json.append({"username": users[0].get("username"),
-                                      "task": i.get("title"),
-                                      "completed": i.get("completed")})
-            data[u] = list_json
-        json.dump(data, file)
+                    data.append({"username": users[0].get("username"),
+                                 "task": i.get("title"),
+                                 "completed": i.get("completed")})
+            list_json[u] = data
+        json.dump(list_json, f)
 
 if __name__ == "__main__":
     """[Function initials]"""
-    users = requests.get("https://jsonplaceholder.typicode.com/users").json()
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos").json()
-    f_json(users, todos)
+    users = requests.get("https://jsonplaceholder.typicode.com/users/").json()
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
+
+    make_all(users, todos)
