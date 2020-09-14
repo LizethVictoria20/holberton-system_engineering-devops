9f146d36bddbd96dd6c5277df90f79698e5a3436
LizethVictoria20 <lizvictoria754@gmail.com>
2020-09-14 23:39:44 +0000

    Update

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,21 +8,22 @@ import sys
 def f_json(users=None, todos=None):
     """Export to CSV"""
     data = []
-    list_json = {}
-    with open("todo_all_employees.json", "w") as f:
+    user_id = {}
+    with open("todo_all_employees.json", "w") as file:
         for i in users:
-            u = i.get("id")
+            user = i.get("id")
             for i in todos:
-                if u == i.get("userId"):
+                if user == i.get("userId"):
                     data.append({"username": users[0].get("username"),
                                  "task": i.get("title"),
                                  "completed": i.get("completed")})
-            list_json[u] = data
-        json.dump(list_json, f)
+
+            user_id[user] = data
+        json.dump(user_id, file)
 
 if __name__ == "__main__":
     """[Function initials]"""
     users = requests.get("https://jsonplaceholder.typicode.com/users/").json()
     todos = requests.get("https://jsonplaceholder.typicode.com/todos/").json()
 
-    make_all(users, todos)
+    f_json(users, todos)
