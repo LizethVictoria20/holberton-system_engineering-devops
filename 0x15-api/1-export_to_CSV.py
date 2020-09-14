632dbaf972418f217721cdec65131eecc9e77b17
LizethVictoria20 <lizvictoria754@gmail.com>
2020-09-14 22:11:40 +0000

    Update

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,9 +9,9 @@ def f_csv(users=None, todos=None):
     """Export to CSV"""
     data = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
 
-    with open(sys.argv[1] + ".csv", "w") as File:
-        write = csv.DictWriter(File, fieldnames=data, quoting=csv.QUOTE_ALL)
-        for row in todos:
+    with open(sys.argv[1] + ".csv", "w") as file:
+        write = csv.DictWriter(file, fieldnames=data, quoting=csv.QUOTE_ALL)
+        for i in todos:
             write.writerow({"USER_ID": i.get("userId"),
                             "USERNAME": users[0].get("username"),
                             "TASK_COMPLETED_STATUS": i.get("completed"),
@@ -20,22 +20,16 @@ def f_csv(users=None, todos=None):
 if __name__ == "__main__":
     """[Function initial]"""
     if len(sys.argv) == 2 and sys.argv[1].isdigit():
-        args = {"id": sys.argv[1]}
+        args_id = {"id": sys.argv[1]}
         users = requests.get("https://jsonplaceholder.typicode.com/users",
-                             params=args).json()
-        args = {"userId": sys.argv[1]}
+                             params=args_id).json()
+        args_userid = {"userId": sys.argv[1]}
         todos = requests.get("https://jsonplaceholder.typicode.com/todos",
-                             params=args).json()
+                             params=args_userid).json()
         done_task = 0
         number_task = []
         for i in todos:
             if i.get("completed"):
                 number_task.append(i)
                 done_task += 1
-
-        print("Employee {} is done with tasks({}/{}):".format(
-              users[0].get("name"), done_task, len(todos)))
-
-        for i in number_task:
-            print("\t {}".format(i.get("title")))
-    f_csv(users, todos)
+            f_csv(users, todos)
