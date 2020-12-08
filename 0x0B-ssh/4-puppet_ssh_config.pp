27ff3674e3d495816e74f8e5e4afb82b7e605931
LizethVictoria20 <lizvictoria754@gmail.com>
2020-12-08 12:55:03 +0000

    Update

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Client configuration file...
-file_line {
+file_line { 'Refuse to authenticate using a password':
   ensure => 'present',
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no'
 }
-file_line {
-  ensure => 'present'
+file_line { 'configuration to use the private key':
+  ensure => 'present',
   path => '/etc/ssh/ssh_config',
-  line => ' IdentityFile /home/vagrant/.ssh/holberton'
+  line => ' IdentityFile ~/.ssh/holberton'
 }
