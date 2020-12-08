4c31857bbe670e870d7444d45cfab3776f6f798e
LizethVictoria20 <lizvictoria754@gmail.com>
2020-12-08 12:52:11 +0000

    Update

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,11 @@
 # Client configuration file...
 file_line {
-  ensure => 'present'
-  path => '/etc/ssh/ssh_config',
-  line => ' IdentityFile /home/vagrant/.ssh'
-}
-file_line {
   ensure => 'present',
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no'
+}
+file_line {
+  ensure => 'present'
+  path => '/etc/ssh/ssh_config',
+  line => ' IdentityFile /home/vagrant/.ssh'
 }
