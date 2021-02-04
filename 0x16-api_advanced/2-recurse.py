85cc880065f43dcf2885a5b41bf752723a07715b
LizethVictoria20 <lizvictoria754@gmail.com>
2021-02-04 18:06:49 +0000

    Update list

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -2,26 +2,27 @@
 """
     Funtion Sub
 """
+import json
 import requests
+url = 'https://www.reddit.com/'
+headers = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6)\
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"}
 
 
-def recurse(subreddit, hot_list=[], after=""):
-    """[recurse]"""
+def recurse(subreddit, hot_list=[], after=' '):
+    query = '/hot.json?limit=100&after='
+    url_subreddit = (url + '/r/' + subreddit + query + after)
 
-    url_base = 'http://www.reddit.com/r/'
-    url_query = '{:s}/hot.json'.format(subreddit)
-    headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6)\
-    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36'}
-    r = requests.get(url_base + url_query, headers=headers)
-
-    if (r.status_code is 302):
-        print("None")
-        return
-    if (r.status_code is 404):
+    r_subreddit = requests.get(url_subreddit, headers=headers)
+    if r_subreddit.status_code != 200:
         return None
-    else:
-        r = r.json()
-        for post in r['data']['children']:
-            hot_list.append(post['data']['title'])
 
+    obj_subreddit = r_subreddit.json()
+
+    for children in obj_subreddit['data']['children']:
+        hot_list.append(children['data']['title'])
+
+    after = obj_subreddit['data']['after']
+    if after is None:
+        return hot_list
     return recurse(subreddit, hot_list, after)
