12e2b81df98e1ef3633ac47becaec230b6c695d8
LizethVictoria20 <lizvictoria754@gmail.com>
2021-02-04 17:47:38 +0000

    Update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import requests
+"""[Subs]"""
 
 
 def number_of_subscribers(subreddit):
     """Subreddit"""
+    userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6)\
+    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"
     url = "https://api.reddit.com/r/{}/about".format(subreddit)
-    head = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"}
+    head = {"User-Agent": userAgent}
     request = requests.get(url, headers=head)
     if request.status_code != 200:
         return 0
